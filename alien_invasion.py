9842918efc871d916011cc22528a1460c2c823fd
Adolfo <psyal3@nottingham.ac.uk>
2020-07-16 19:22:18 +0200

    AlienInvasion class created

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,3 +1,22 @@
 import sys
 import pygame
 
+# Overall class to manage game assets and behaviour
+class AlienInvasion:
+    # initialise game and resources
+    def __init__(self):
+        pygame.init()
+
+        self.screen = pygame.display.set_mode((1200, 800))
+        pygame.display.set_caption("Alien Invasion by adolfolh")
+
+    # start main loop for the game
+    def run_game(self):
+        # watch mouse and keys events
+        while True:
+            for event in pygame.event.get():
+                if event.type() == pygame.QUIT:
+                    sys.exit()
+
+        # make the most recently drawn screen visible
+        pygame.display.flip()
