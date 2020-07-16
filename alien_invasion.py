48b68b30b54b7edc46e1b2ac919a31306973c4e2
Adolfo <psyal3@nottingham.ac.uk>
2020-07-16 22:33:43 +0200

    changing the bg color

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,14 +1,18 @@
 import sys
 import pygame
 
+
 # Overall class to manage game assets and behaviour
 class AlienInvasion:
     # initialise game and resources
     def __init__(self):
         pygame.init()
 
+        # set bg color
+        self.bg_color = (230, 230, 230)
+
         self.screen = pygame.display.set_mode((1200, 800))
-        pygame.display.set_caption("Alien Invasion by adolfolh")
+        pygame.display.set_caption("Alien Invasion by github.com/adolfolh")
 
     # start main loop for the game
     def run_game(self):
@@ -18,8 +22,12 @@ class AlienInvasion:
                 if event.type == pygame.QUIT:
                     sys.exit()
 
-        # make the most recently drawn screen visible
-        pygame.display.flip()
+            # redraw the screen during each pass through the loop
+            self.screen.fill(self.bg_color)
+
+            # make the most recently drawn screen visible
+            pygame.display.flip()
+
 
 # make a game instance and run the game
 if __name__ == "__main__":
